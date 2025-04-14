922c8cb95244f991198afeb91d2e8cd4ba55e518
Edson Rocha <115895616+dsnfeels@users.noreply.github.com>
2025-04-14 15:56:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def root():
     return {"message": "Hello World"}
 
 ##http://127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/teste")
 async def funcaoteste():
     return  {"teste": True, "num_aleatoriou": random.randint(0, 1000)}
 
